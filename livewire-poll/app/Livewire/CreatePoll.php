ca3db7277ca2b104689d4f5da6f6ccaf3e63fd68
Tobias Nerger <tobias.nerger@gmail.com>
2025-07-01 12:12:35 +0200

    Enhance poll creation with validation and error messages for title and options

--- livewire-poll/app/Livewire/CreatePoll.php
+++ livewire-poll/app/Livewire/CreatePoll.php
@@ -12,6 +12,17 @@ class CreatePoll extends Component
     public $title;
     public $options = ['First Option'];
 
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'options' => 'required|array|min:1|max:10',
+        'options.*' => 'required|min:1|max:255' //Validierung für jedes Item im Array
+
+    ];
+
+    protected $messages = [
+        'options.*' => "The options can't be empty."
+    ];
+
     public function render()
     {
         return view('livewire.create-poll');
@@ -30,17 +41,31 @@ class CreatePoll extends Component
         $this->options = array_values($this->options);
     }
 
+    public function updated($propName)
+    {
+        $this->validateOnly($propName);
+    }
+
     public function createPoll()
     {
-        $poll = Poll::create([
+        $this->validate();
+        Poll::create([
             'title' => $this->title
-        ]);
+        ])->options()
+            // createMany ermöglicht es eine Kollektion zurückzugeben
+            // und so viele Optionen gleichezitig zu erzeugen
+            ->createMany(
+                collect($this->options) // Kollektion erzeugen
+                    ->map(fn($option) => ['name' => $option]) // Ein Assoc Array erzeugen
+                    ->all() // Array zurückgeben
+            );
 
-        foreach ($this->options as $optionName) {
-            $poll->options()->create(['name' => $optionName]);
-        }
+        // Oder einfach Oldschool:
+        // foreach ($this->options as $optionName) {
+        //     $poll->options()->create(['name' => $optionName]);
+        // }
 
-        $this->reset(['title','options']);
+        $this->reset(['title', 'options']);
     }
 
     // public function mount()
--- livewire-poll/resources/views/livewire/create-poll.blade.php
+++ livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -2,6 +2,9 @@
     <form wire:submit.prevent="createPoll">
         <label for="">Poll Title</label>
         <input type="text" wire:model.live="title" />
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
 
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
@@ -15,6 +18,9 @@
                         <input type="text" wire:model="options.{{ $index }}">
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error('options.' . $index)
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
