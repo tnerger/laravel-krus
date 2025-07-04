6c2d89a1f09767e77a4c0f437e977b1c2800174e
Tobias Nerger <tobias.nerger@gmail.com>
2025-07-04 12:26:47 +0200

    Add TextInput component and update job index view with search and salary filters

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -1,5 +1,22 @@
 <x-layout>
-    <x-bread-crumbs class="mb-4" :links="['Jobs' => '#' ]" />
+    <x-bread-crumbs class="mb-4" :links="['Jobs' => '#']" />
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                <x-text-input name="search" value="" placeholder="Search for any Text" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+                <div class="flex space-x-2">
+                    <x-text-input name="min_salary" value="" placeholder="From" />
+                    <x-text-input name="max_salary" value="" placeholder="To" />
+                </div>
+                <div>xxx</div>
+                <div>xxx</div>
+            </div>
+
+    </x-card>
     @foreach ($jobs as $job)
         <x-job-card :$job>
             <div>
---
+++ job-board/tailwind.config.js
@@ -0,0 +1,15 @@
+import forms from '@tailwindcss/forms';
+
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms(),
+  ],
+}
