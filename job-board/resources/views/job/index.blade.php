8a15f38a1db9913c0255a1a91c2e535d04b8ce6f
Tobias Nerger <tobias.nerger@gmail.com>
2025-07-05 08:04:41 +0200

    Add form reference to TextInput component and update job index view for improved form handling - Alpine.js added

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
     <x-bread-crumbs class="mb-4" :links="['Jobs' => '#']" />
-    <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+    <x-card class="mb-4 text-sm" x-data="{}">
+        <form x-ref="filters" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any Text" />
+                    <x-text-input name="search" value="{{ request('search') }}" form-ref="filters" placeholder="Search for any Text" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" form-ref="filters" placeholder="From" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" form-ref="filters" placeholder="To" />
                     </div>
                 </div>
                 <div>
